1a7a7b54808ee32b2cd3705a3f144959c09d2079
Seondong Pyo <psd1390@gmail.com>
2020-11-12 22:13:02 +0900

    일대일 연관관계 매핑 - 대상 테이블에 외래 키가 있는 경우

--- src/main/java/com/hello/entity/Locker.java
+++ src/main/java/com/hello/entity/Locker.java
@@ -1,9 +1,6 @@
 package com.hello.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 
 @Entity
 public class Locker {
@@ -13,7 +10,10 @@ public class Locker {
 
     private String name;
 
-    // 일대일 양방향 매핑 시 : 다대일 양방향처럼 연관관계의 주인을 명시한다
-    @OneToOne(mappedBy = "locker")
+    // 외래 키가 대상 테이블에 존재하는 일대일 양방향 연관관계 매핑의 경우
+    //  - 외래 키를 Locker에서 관리하므로 연관관계의 주인은 Locker가 된다
+    @OneToOne
+    @JoinColumn(name = "MEMBER_ID")
+//    @OneToOne(mappedBy = "locker") // 일대일 양방향 매핑 시 : 다대일 양방향처럼 연관관계의 주인을 명시한다
     private Member member;
 }
