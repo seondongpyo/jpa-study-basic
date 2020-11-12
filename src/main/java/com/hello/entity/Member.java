12c6adfaa65339d70bb075ae8ccde9d9f4e4f203
Seondong Pyo <psd1390@gmail.com>
2020-11-12 21:40:30 +0900

    일대다 양방향 연관관계 매핑

--- src/main/java/com/hello/entity/Member.java
+++ src/main/java/com/hello/entity/Member.java
@@ -13,6 +13,14 @@ public class Member {
     @Column(name = "USERNAME")
     private String username;
 
+    // 일대다 양방향 연관관계 매핑
+    //  - 이런 매핑은 공식적으로 존재하지 않는다
+    //  - 읽기 전용 필드를 사용해서 양방향처럼 사용하는 방법이다
+    //  => 다대일 양방향을 사용하자
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    private Team team;
+
     public Long getId() {
         return id;
     }
