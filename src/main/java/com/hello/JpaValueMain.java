5a71a226da8cccf844af1a6978b6c3f9bac9c408
Seondong Pyo <psd1390@real-iv.co.kr>
2020-11-18 08:24:28 +0900

    값 타입과 불변 객체

--- src/main/java/com/hello/JpaValueMain.java
+++ src/main/java/com/hello/JpaValueMain.java
@@ -9,7 +9,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
-public class JpaValueMain {
+public class  JpaValueMain {
 
     /*
         << JPA의 데이터 타입 분류 >>
@@ -61,11 +61,57 @@ public class JpaValueMain {
         transaction.begin();
 
         try {
-            Member member = new Member();
-            member.setUsername("memberA");
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
-            member.setWorkPeriod(new Period()); // 임베디드 타입의 값이 null이면 매핑한 컬럼의 값들도 모두 null
-            em.persist(member);
+            Address address = new Address("city", "street", "zipcode");
+
+            Member member1 = new Member();
+            member1.setUsername("memberA");
+            member1.setHomeAddress(address);
+            member1.setWorkPeriod(new Period()); // 임베디드 타입의 값이 null이면 매핑한 컬럼의 값들도 모두 null
+            em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("memberB");
+            member2.setHomeAddress(address); // 임베디드 타입 같은 값 타입을 여러 엔티티에서 공유하면 위험하다!
+            member2.setWorkPeriod(new Period());
+            em.persist(member2);
+
+            // 값 타입 공유에 의한 부작용(side effect) 발생
+//            member1.getHomeAddress().setCity("newCity"); // member1과 member2 모두 update 쿼리가 호출된다
+
+            // (당연한 말이지만) 영속성 컨텍스트를 비운 뒤, 하나의 멤버만 조회하여 수정하면 가능
+//            em.flush();
+//            em.clear();
+//            Member findMember1 = em.find(Member.class, member1.getId());
+//            findMember1.getHomeAddress().setCity("newCity"); // update 쿼리가 해당 엔티티에 대해서만 호출된다
+
+            // !! 값 타입의 실제 인스턴스인 값을 공유하는 것은 위험하기 때문에, 값(인스턴스)을 복사해서 사용해야 한다 !!
+//            Address newAddress = new Address(address.getCity(), address.getStreet(), address.getZipcode());
+//            member2.setHomeAddress(newAddress);
+
+            /*
+                << 객체 타입의 한계 >>
+                - 항상 값을 복사해서 사용하면 공유 참조로 인해 발생하는 부작용을 피할 수 있다.
+                - 하지만 임베디드 타입처럼 사용자가 직접 정의한 값 타입은, 자바의 기본 타입이 아닌 객체 타입이다.
+                - 자바의 기본 타입에 값을 대입하면 값을 복사하기 때문에 안전하지만, 객체는 참조 값을 대입하므로 위험하다.
+                - 객체 타입은 참조 값을 직접 대입하는 것을 막을 방법이 없으므로 객체의 공유 참조는 피할 수 없다.
+                    => 객체 타입을 수정할 수 없게, 즉 불변 객체로 만들면 된다!
+
+                << 불변 객체 >>
+                - 생성 시점 이후 절대 값을 변경할 수 없는 객체를 의미한다.
+                - 값 타입은 불변 객체(immutable object)로 설계해야 한다.
+                - 생성자로만 값을 설정하고, 수정자(setter)를 만들지 않거나 private하게 만들면 된다.
+                   (참고) Integer, String은 자바가 제공하는 대표적인 불변 객체
+                    => '불변'이라는 작은 제약을 통해 '부작용'이라는 큰 재앙을 막을 수 있다.
+             */
+
+            // Q. 그럼 수정자가 없는데, 값을 바꾸고 싶으면 어떻게 해야 하나?
+            //  => 값을 완전히 새로 생성하여 넣어준다.
+            Member member3 = new Member();
+            member3.setHomeAddress(address);
+            em.persist(member3);
+
+            Address newAddress = new Address("newCity", address.getStreet(), address.getZipcode());
+            member3.setHomeAddress(newAddress);
 
             transaction.commit();
 
